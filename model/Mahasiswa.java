266aab37e05bedba7ebed66b04d8233cb3bf719a
Norvitas <noorvitasari7@gmail.com>
2020-04-22 07:21:27 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("==== Data Mahasiswa ===========");
+  System.out.println("NPM  : 18630827");
+  System.out.println("Nama : Noor Vita Sari");
+  System.out.println("===============================");
+ }
+}
